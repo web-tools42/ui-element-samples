b537e72d13e1625098416eb3a214ae6d12a0be52
Nito Buendia <effectedcard@gmail.com>
2017-11-03 23:09:55 +0800

    Clean and comment code. (#105)
    * Update index.html
    
    * Moving CSS to external file
    
    This allows to focus on the WebWorker example and reduces noise.
    
    * Create main.css
    
    Creating file to contain all CSS.
    
    Additional changes made:
    - Add margin-top for canvas a replacement for <br>
    - Add spaces after ":".
    - Change 0px to 0.
    - Order alphabetically.
    
    * Add comments on worker.js
    
    * Update index.html
    
    * Update index.html

--- web-workers/worker.js
+++ web-workers/worker.js
@@ -11,17 +11,25 @@
   limitations under the License.
 */
 
-addEventListener('message', (d) => {
-  const imageData = d.data
-  const w = imageData.width
-  const h = imageData.height
-  const data = imageData.data
 
+/**
+ * Handles incoming messages in the web worker.
+ * In this case: increases each pixel's red color by 20%.
+ * @param {!Object} d Incoming data.
+ */
+addEventListener('message', (d) => {
+  const imageData = d.data;
+  const w = imageData.width;
+  const h = imageData.height;
+  const data = imageData.data;
+
+  // Iterate pixel rows and columns to change red color of each.
   for (let x = 0; x < w; x++) {
     for (let y = 0; y < h; y++) {
-      let index = (x + (y * w)) * 4
-      data[index] = data[index] * 1.2
+      let index = (x + (y * w)) * 4;
+      data[index] = data[index] * 1.2;
     }
   }
+  
   postMessage(imageData, [imageData.data.buffer])
-})
+});
